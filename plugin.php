58fd8db52fd99ecd3a6e7463fabd17b98c5681f6
Khoi <khoii87@gmail.com>
2020-07-05 17:27:34 +0200

    Format: :art: Refactor the code to removing unnecessarily function in PHP to load register block type

--- plugin.php
+++ plugin.php
@@ -13,22 +13,6 @@ if(!defined('ABSPATH')) {
     exit;
 }
 
-function gu_register_block_type($blockName, $options = array()) {
-    register_block_type(
-        'dekode/' . $blockName,
-        array_merge(
-            array(
-                'editor_script' => 'gu-block-editor-script', // This will be JS file that is only going to be enqueued in the admin editor page
-                'editor_style'  => 'gu-block-editor-style', // This will be our CSS that is only going to enqueued in the admin editor page
-                // These scripts will be enqueued both in back- and front-end
-                'script' => 'gu-block-editor-script', 
-                'style' => 'gu-block-editor-style'
-            ),
-            $options
-        )
-    ); 
-}
-
 function de_gutenberg_api_fnugg() {
 
     // Register JavaScript Section
@@ -45,7 +29,17 @@ function de_gutenberg_api_fnugg() {
          array('wp-edit-blocks')
     );
 
-    gu_register_block_type('api-fnugg');
+     register_block_type(
+        'dekode/api-fnugg',
+        array(
+            'editor_script' => 'gu-block-editor-script', // This will be JS file that is only going to be enqueued in the admin editor page
+            'editor_style'  => 'gu-block-editor-style', // This will be our CSS that is only going to enqueued in the admin editor page
+            // These scripts will be enqueued both in back- and front-end
+            'script' => 'gu-block-editor-script', 
+            'style' => 'gu-block-editor-style'
+        )
+    ); 
+     
 }
 
 
